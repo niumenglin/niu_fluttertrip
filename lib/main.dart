f9b47fe684b778821c372ab9f3afdaff5416dba6
牛梦林 <799978248@qq.com>
2022-02-21 14:30:31 +0800

    feat: 沉浸式状态栏

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:niu_fluttertrip/navigator/tab_navigator.dart';
 
 void main() {
+  setSystemUIOverlayStyle();
   runApp(const MyApp());
+}
+
+void setSystemUIOverlayStyle() {
+  if (Platform.isAndroid) {
+    SystemUiOverlayStyle systemUiOverlayStyle = const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+
+        ///这是设置状态栏的图标和字体的颜色
+        ///Brightness.light  一般都是显示为白色;Brightness.dark 一般都是显示为黑色
+        statusBarIconBrightness: Brightness.dark);
+    SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+  }
 }
 
 class MyApp extends StatelessWidget {
