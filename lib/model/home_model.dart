944fa564b0d22ba7ec556bff88d1fb563f4ea1d2
牛梦林 <799978248@qq.com>
2022-02-22 11:36:46 +0800

    feat: 首页大接口Model层设计

---
+++ lib/model/home_model.dart
@@ -0,0 +1,45 @@
+import 'package:niu_fluttertrip/model/common_model.dart';
+import 'package:niu_fluttertrip/model/config_model.dart';
+import 'package:niu_fluttertrip/model/gird_nav_model.dart';
+import 'package:niu_fluttertrip/model/sales_box_model.dart';
+
+///首页模型
+class HomeModel {
+  final ConfigModel? config;
+  final List<CommonModel>? bannerList;
+  final List<CommonModel>? localNavList;
+  final List<CommonModel>? subNavList;
+  final GridNavModel? gridNav;
+  final SalesBoxModel? salesBox;
+
+  HomeModel(
+      {this.config,
+      this.bannerList,
+      this.localNavList,
+      this.subNavList,
+      this.gridNav,
+      this.salesBox});
+
+  factory HomeModel.fromJson(Map<String, dynamic> json) {
+    var bannerListJson = json['bannerList'] as List;
+    List<CommonModel> bannerList =
+        bannerListJson.map((e) => CommonModel.fromJson(e)).toList();
+
+    var localNavListJson = json['localNavList'] as List;
+    List<CommonModel> localNavList =
+        localNavListJson.map((e) => CommonModel.fromJson(e)).toList();
+
+    var subNavListJson = json['subNavList'] as List;
+    List<CommonModel> subNavList =
+        subNavListJson.map((e) => CommonModel.fromJson(e)).toList();
+
+    return HomeModel(
+      bannerList: bannerList,
+      localNavList: localNavList,
+      subNavList: subNavList,
+      config: ConfigModel.fromJson(json['config']),
+      gridNav: GridNavModel.fromJson(json['gridNav']),
+      salesBox: SalesBoxModel.fromJson(json['salesBox']),
+    );
+  }
+}
