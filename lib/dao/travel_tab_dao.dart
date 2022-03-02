a59019cdf96dfe3b4373e5c33dd1127d33480aee
牛梦林 <799978248@qq.com>
2022-03-02 16:37:31 +0800

    feat: 旅拍模块Dao层设计

--- lib/dao/travel_tab_dao.dart
+++ lib/dao/travel_tab_dao.dart
@@ -1,44 +1,20 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:niu_fluttertrip/model/travel_model.dart';
+import 'package:niu_fluttertrip/model/travel_tab_model.dart';
 
-const TRAVEL_URL = 'http://www.devio.org/io/flutter_app/json/travel_page.json';
+const TRAVEL_TAB_URL =
+    'http://www.devio.org/io/flutter_app/json/travel_page.json';
 
 ///旅拍类别接口
-var Params = {
-  "districtId": -1,
-  "groupChannelCode": "RX-OMF",
-  "type": null,
-  "lat": -180,
-  "lon": -180,
-  "locatedDistrictId": 0,
-  "pagePara": {
-    "pageIndex": 1,
-    "pageSize": 10,
-    "sortType": 9,
-    "sortDirection": 0
-  },
-  "imageCutType": 1,
-  "head": {},
-  "contentType": "json"
-};
-
 class TravelTabDao {
-  static Future<TravelModel?> fetch(
-      String url, String groupChannelCode, int pageIndex, int pageSize) async {
-    //动态修改固定参数
-    Map paramsMap = Params['pagePara'] as Map;
-    paramsMap['groupChannelCode'] = groupChannelCode;
-    paramsMap['pageIndex'] = pageIndex;
-    paramsMap['pageSize'] = pageSize;
-    final response =
-        await http.post(Uri.parse(TRAVEL_URL), body: jsonEncode(Params));
+  static Future<TravelTabModel?> fetch() async {
+    final response = await http.get(Uri.parse(TRAVEL_TAB_URL));
     if (response.statusCode == 200) {
       //success
       Utf8Decoder utf8decoder = const Utf8Decoder(); //fix 中文乱码
       var result = json.decode(utf8decoder.convert(response.bodyBytes));
-      return TravelModel.fromJson(result);
+      return TravelTabModel.fromJson(result);
     } else {
       throw Exception('Fail to load travel');
     }
