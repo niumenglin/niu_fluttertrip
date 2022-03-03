6ebffdbc9b0fa5986c9bff1b6db37d1b7b0a7054
牛梦林 <799978248@qq.com>
2022-03-03 16:58:07 +0800

    feat: 实现旅拍卡片布局

--- lib/pages/travel_tab_page.dart
+++ lib/pages/travel_tab_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:niu_fluttertrip/dao/travel_dao.dart';
 import 'package:niu_fluttertrip/model/travel_model.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:niu_fluttertrip/widgets/webview.dart';
 
 ///旅拍Tab
 const int PAGE_SIZE = 10;
@@ -19,9 +20,14 @@ class TravelTabPage extends StatefulWidget {
   _TravelTabPageState createState() => _TravelTabPageState();
 }
 
-class _TravelTabPageState extends State<TravelTabPage> {
+class _TravelTabPageState extends State<TravelTabPage>
+    with AutomaticKeepAliveClientMixin {
   List<TravelItem> travelItems = [];
   int pageIndex = 1;
+
+  // true=当前页面保活（with AutomaticKeepAliveClientMixin）
+  @override
+  bool get wantKeepAlive => true;
 
   @override
   void initState() {
@@ -97,6 +103,137 @@ class ItemTravel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('$index');
+    return GestureDetector(
+        onTap: () {
+          if (item.article!.urls != null && item.article!.urls!.isNotEmpty) {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => WebView(
+                          url: item.article?.urls![0].h5Url,
+                          title: '详情',
+                        )));
+          }
+        },
+        child: Card(
+          child: PhysicalModel(
+            color: Colors.transparent,
+            clipBehavior: Clip.antiAlias,
+            borderRadius: BorderRadius.circular(5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _itemImage(),
+                Container(
+                  padding: EdgeInsets.all(4),
+                  child: Text(
+                    item.article!.articleTitle ?? '',
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(fontSize: 14, color: Colors.black87),
+                  ),
+                ),
+                _infoText()
+              ],
+            ),
+          ),
+        ));
+  }
+
+  _itemImage() {
+    return Stack(
+      children: [
+        Image.network(item.article!.images![0].dynamicUrl ?? ''),
+        Positioned(
+          left: 8,
+          bottom: 8,
+          child: Container(
+            padding: EdgeInsets.fromLTRB(5, 1, 5, 1),
+            decoration: BoxDecoration(
+                color: Colors.black54, borderRadius: BorderRadius.circular(10)),
+            child: Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(right: 3),
+                  child: Icon(
+                    Icons.location_on,
+                    color: Colors.white,
+                    size: 12,
+                  ),
+                ),
+                LimitedBox(
+                  maxWidth: 130,
+                  child: Text(
+                    _poiName(),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(fontSize: 12, color: Colors.white),
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  //位置信息
+  String _poiName() {
+    return item.article!.pois == null || item.article!.pois!.isEmpty
+        ? '未知'
+        : item.article!.pois![0].poiName ?? '未知';
+  }
+
+  _infoText() {
+    return Container(
+      padding: EdgeInsets.fromLTRB(6, 0, 6, 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              //裁剪Widget
+              PhysicalModel(
+                color: Colors.transparent,
+                clipBehavior: Clip.antiAlias,
+                borderRadius: BorderRadius.circular(12), //圆角角度是 宽高一半 才能裁出圆形
+                child: Image.network(
+                  item.article?.author?.coverImage?.dynamicUrl ?? '',
+                  width: 24,
+                  height: 24,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(5),
+                width: 90,
+                child: Text(
+                  item.article?.author?.nickName ?? '',
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontSize: 12, color: Colors.black87),
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Icon(
+                Icons.thumb_up,
+                size: 14,
+                color: Colors.grey,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 3),
+                child: Text(
+                  item.article?.likeCount.toString() ?? '',
+                  style: TextStyle(fontSize: 10),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
