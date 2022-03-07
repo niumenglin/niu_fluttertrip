03dfb4c152b882b000ad25fe4ab232fe78d9e3e6
牛梦林 <799978248@qq.com>
2022-03-07 22:05:15 +0800

    feat: Flutter语音插件-Android端实现

--- android/app/src/main/kotlin/com/niu/niu_fluttertrip/MainActivity.kt
+++ android/app/src/main/kotlin/com/niu/niu_fluttertrip/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.niu.niu_fluttertrip
 
 import android.os.Bundle
+import com.niu.flutter.plugin.asr.AsrPlugin
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 import org.devio.flutter.splashscreen.SplashScreen
 
 class MainActivity: FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         SplashScreen.show(this, true) // here
         super.onCreate(savedInstanceState)
+    }
+
+    /**
+     * 注册自定义Native插件
+     */
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+        //flutter sdk >= v1.17.0 时使用下面方法注册自定义plugin
+        AsrPlugin.registerWith(this, flutterEngine.dartExecutor.binaryMessenger)
     }
 }
