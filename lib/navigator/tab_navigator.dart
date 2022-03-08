88f365fad8d5ff8414554832b64c028d68d33d78
牛梦林 <799978248@qq.com>
2022-03-08 22:06:39 +0800

    refactor: 再按一次退出APP功能(间隔1秒内)

--- lib/navigator/tab_navigator.dart
+++ lib/navigator/tab_navigator.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:niu_fluttertrip/pages/home_page.dart';
 import 'package:niu_fluttertrip/pages/my_page.dart';
 import 'package:niu_fluttertrip/pages/search_page.dart';
 import 'package:niu_fluttertrip/pages/travel_page.dart';
 
 class TabNavigator extends StatefulWidget {
+  const TabNavigator({Key? key}) : super(key: key);
+
   @override
   _TabNavigatorState createState() => _TabNavigatorState();
 }
@@ -14,41 +17,61 @@ class _TabNavigatorState extends State<TabNavigator> {
   final _activeColor = Colors.blue;
   int _currentIndex = 0;
   final PageController _controller = PageController(initialPage: 0);
+  DateTime? _lastClickTime;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(
-        controller: _controller,
-        physics: const NeverScrollableScrollPhysics(), //禁止左右滑动
-        onPageChanged: (int index) {
-          print('当前页面下标是：$index');
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        children: [
-          HomePage(),
-          SearchPage(),
-          TravelPage(),
-          MyPage(),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (index) {
-          _controller.jumpToPage(index);
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        type: BottomNavigationBarType.fixed,
-        items: [
-          _bottomNavigationBarItem(Icons.home, '首页', 0),
-          _bottomNavigationBarItem(Icons.search, '搜索', 1),
-          _bottomNavigationBarItem(Icons.camera_alt, '旅拍', 2),
-          _bottomNavigationBarItem(Icons.account_circle, '我的', 3),
-        ],
+    return WillPopScope(
+      onWillPop: () {
+        if (_lastClickTime == null ||
+            DateTime.now().difference(_lastClickTime!) >
+                const Duration(seconds: 1)) {
+          //两次点击时间间隔超过1秒则重新计时
+          _lastClickTime = DateTime.now();
+          Fluttertoast.showToast(
+              msg: "再按一次退出APP",
+              textColor: Colors.white,
+              gravity: ToastGravity.CENTER,
+              backgroundColor: Colors.black54);
+          return Future.value(false); //不消费
+        }
+        Fluttertoast.cancel();
+        return Future.value(true);
+      },
+      child: Scaffold(
+        //用WillPopScope组件把你的页面body部分包起来，它可以拦截返回键按钮进行误触判断
+        body: PageView(
+          controller: _controller,
+          physics: const NeverScrollableScrollPhysics(), //禁止左右滑动
+          onPageChanged: (int index) {
+            print('当前页面下标是：$index');
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          children: [
+            HomePage(),
+            SearchPage(),
+            TravelPage(),
+            MyPage(),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            _controller.jumpToPage(index);
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          type: BottomNavigationBarType.fixed,
+          items: [
+            _bottomNavigationBarItem(Icons.home, '首页', 0),
+            _bottomNavigationBarItem(Icons.search, '搜索', 1),
+            _bottomNavigationBarItem(Icons.camera_alt, '旅拍', 2),
+            _bottomNavigationBarItem(Icons.account_circle, '我的', 3),
+          ],
+        ),
       ),
     );
   }
